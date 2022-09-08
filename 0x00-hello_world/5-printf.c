bc98f12e657c7085639d5e1f00e3fb34c23a55d0
Abel Morris Ben <abelmorrisben@icloud.com>
2022-09-08 12:50:36 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  * main - The entry point of C programs
+  * @void: The function takes no parameters
+  *
+  * Description: The function "printf" writes to stdout.
+  * Return: 0
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
