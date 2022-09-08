bdd4155fb5199e4dca717f098b838764072dca1e
Abel Morris Ben <abelmorrisben@icloud.com>
2022-09-08 12:49:13 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  * main - The entry point of C programs
+  * @void: The function takes no parameters
+  *
+  * Description: The function "puts" writes to stdout.
+  * Return: 0
+  */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
