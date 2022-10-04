220ff9ee75a18597094c0adc1d0edc7186ed5587
Abel Morris Ben <abelmorrisben@icloud.com>
2022-10-04 12:03:50 -0700

    alloc grid #2

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,45 +1,21 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-
 /**
- * alloc_grid - create a 2d array of ints
- * @width: width of grid
- * @height: height of grid
- * Description: creat a grid, initialize to 0
- * Return: 2d array, NULL if fail
- **/
-int **alloc_grid(int width, int height)
+* free_grid - A function that frees a 2D array of ints previously
+* created by your malloc_grid function
+* @grid: An input 2D array of integers to free
+* @height: height of grid
+* Return: Nothing
+*/
+
+void free_grid(int **grid, int height)
 {
-	int **grid;
-	int x, y;
-
-	if (width <= 0 || height <= 0)
-		return (NULL);
-
-	grid = (int **)malloc(height * sizeof(int *));
-
+	int i = 0;
+	
 	if (grid == NULL)
-		return (NULL);
+		return;
 
-	for (x = 0; x < height; x++)
-	{
-		grid[x] = (int *)malloc(width * sizeof(int));
-		if (grid[x] == NULL)
-		{
-			for (y = 0; y <= x; y++)
-				free(grid[y]);
-			free(grid);
-			return (NULL);
-		}
-	}
-
-	for (y = 0; y < height; y++)
-	{
-		for (x = 0; x < width; x++)
-		{
-			grid[y][x] = 0;
-		}
-	}
-	return (grid);
+	while (i < height)
+		free((int *)grid[i++]);
+	free(grid);
 }
