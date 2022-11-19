b89290dda0a8e48db953af3e3ceff41f37ea416f
Abel Morris Ben <abelmorrisben@icloud.com>
2022-11-19 02:27:24 -0800

    lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,11 +1,18 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_
+#define _LISTS_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
+ * @len: length of the string
  * @next: points to the next node
  *
+ * Description: singly linked list node structure
+ * for ALX project
  */
 typedef struct list_s
 {
